e50cc28637608e7badd4a910f721ada742b967e1
utkarshbohare <bohareutkarsh@gmail.com>
2024-06-13 18:38:51 +0530

    commit

---
+++ get.js
@@ -0,0 +1,9 @@
+//console.log("hello rjit")
+//alert("hello")
+
+// let data=document.getElementById("a").innerText
+// let data=document.getElementById("a").innerHTML;
+// let data=document.getElementById("a").getAttribute("style");
+// let data=document.getElementById("a").getAttribute("style");
+let data=document.getElementById("a").attributes[0];
+console.log(data);
